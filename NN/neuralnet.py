1895b9ad46620b86c7813c7d1319678a00227f7b
miki <miki@miki>
2024-07-27 22:05:20 +0300

    updated nn

--- NN/neuralnet.py
+++ NN/neuralnet.py
@@ -12,9 +12,23 @@ class DenseLayer:
         """
         """
         self.output = np.dot(inputs, self.weight) + self.biases
+class ReLU:
+    """
+    """
+    def forward(self, inputs):
+        self.output =np.maximum(0, inputs)
+class SoftMax:
+    """
+    """
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.maximum(inputs, axis = 1, keepdims=True))
+        prob = exp_values/np.sum(exp_values, axis=1, keepdims=True)
+        self.outputs = prob
 if __name__ == '__main__':
     # Datasets
     X, y = spiral_data(samples =100, classes=3)
     dense = DenseLayer(2, 3)
+    activation = ReLU()
     dense.forward(X)
-    print(dense.output[:5])
+    activation.forward(dense.output)
+    print(activation.output[:5])
