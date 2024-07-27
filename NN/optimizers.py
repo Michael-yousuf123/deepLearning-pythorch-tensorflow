8358bb8658b3e2d5c28b79d18028114b9e1ba237
miki <miki@miki>
2024-07-28 01:22:06 +0300

    updated sgd

--- NN/optimizers.py
+++ NN/optimizers.py
@@ -6,11 +6,19 @@ from neuralnet import DenseLayer, ReLU
 class SGD:
     """
     """
-    def __init__(self, learning_rate=1):
+    def __init__(self, learning_rate=1, decay = 0):
         self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iteration = 0
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate*(1/(1+self.decay *self.iteration))
     def update_params(self, layer):
-        layer.weight += -self.learning_rate * layer.dweights
-        layer.biases += -self.learning_rate*layer.dbiases
+        layer.weight += -self.current_learning_rate * layer.dweights
+        layer.biases += -self.current_learning_rate*layer.dbiases
+    def post_update(self):
+        self.iteration += 1
 if __name__ == '__main__':
     X, y = spiral_data(samples=100, classes=3)
     dense1 = DenseLayer(2, 64)
