1535919bd17867770b8a2f32780968280991e8ba
miki <miki@miki>
2024-07-28 00:10:33 +0300

    nn update

--- NN/neuralnet.py
+++ NN/neuralnet.py
@@ -11,12 +11,24 @@ class DenseLayer:
     def forward(self, inputs):
         """
         """
+        self.inputs = inputs
         self.output = np.dot(inputs, self.weight) + self.biases
+    def backward(self, dvalues):
+        """
+        """
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        self.dinputs = np.dot(dvalues, self.weight.T)
 class ReLU:
     """
     """
     def forward(self, inputs):
+        self.inputs = inputs
         self.output =np.maximum(0, inputs)
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
+
 class SoftMax:
     """
     """
@@ -24,6 +36,12 @@ class SoftMax:
         exp_values = np.exp(inputs - np.maximum(inputs, axis = 1, keepdims=True))
         prob = exp_values/np.sum(exp_values, axis=1, keepdims=True)
         self.outputs = prob
+    def backward(self, dvalues):
+        self.dinputs = np.empty_like(dvalues)
+        for index, (single_output, single_dvalues) in enumerate(zip(self.outputs, dvalues)):
+            single_output= single_output.reshape(-1,1)
+            jacobian_matrix = np.diagflat(single_output)-np.dot(single_output,single_output.T)
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
 if __name__ == '__main__':
     # Datasets
     X, y = spiral_data(samples =100, classes=3)
--- NN/optimizers.py
+++ NN/optimizers.py
@@ -2,14 +2,36 @@ import numpy as np
 import nnfs
 from nnfs.datasets import spiral_data
 from crossmax import CrosMax
+from neuralnet import DenseLayer, ReLU
 class SGD:
     """
     """
     def __init__(self, learning_rate=1):
         self.learning_rate = learning_rate
     def update_params(self, layer):
-        layer.weights += -self.learning_rate * layer.dweights
+        layer.weight += -self.learning_rate * layer.dweights
         layer.biases += -self.learning_rate*layer.dbiases
 if __name__ == '__main__':
+    X, y = spiral_data(samples=100, classes=3)
+    dense1 = DenseLayer(2, 64)
+    activation1 = ReLU()
+    dense2 = DenseLayer(64, 3)
+    loss_activation = CrosMax()
     optimizer = SGD()
-    optimizer.update_params()
+    for epoch in range(10001):
+        dense1.forward(X)
+        activation1.forward(dense1.output)
+        dense2.forward(activation1.output)
+        loss = loss_activation.forward(dense2.output, y)
+        predictions = np.argmax(loss_activation.output, axis=1)
+        if len(y.shape) == 2:
+            y = np.argmax(y, axis=1)
+        accuracy = np.mean(predictions==y)
+        if not epoch % 100:
+            print(f'epch: {epoch},' + f'acc: {accuracy:.3f}, '+ f'loss: {loss:.3f}')
+        loss_activation.backward(loss_activation.output,y)
+        dense2.backward(loss_activation.dinputs)
+        activation1.backward(dense2.dinputs)
+        dense1.backward(activation1.dinputs)
+        optimizer.update_params(dense1)
+        optimizer.update_params(dense2)
