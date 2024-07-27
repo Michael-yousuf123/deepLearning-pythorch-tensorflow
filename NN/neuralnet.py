1d89507fa52042fcb72326f4b9a6241faecbf2b9
miki <miki@miki>
2024-07-27 21:52:01 +0300

    nn update

--- NN/neuralnet.py
+++ NN/neuralnet.py
@@ -1,6 +1,6 @@
 import numpy as np
 import nnfs
-from nnfs import spiral_data
+from nnfs.datasets import spiral_data
 
 class DenseLayer:
     def __init__(self, ninputs, nneurons):
@@ -12,3 +12,9 @@ class DenseLayer:
         """
         """
         self.output = np.dot(inputs, self.weight) + self.biases
+if __name__ == '__main__':
+    # Datasets
+    X, y = spiral_data(samples =100, classes=3)
+    dense = DenseLayer(2, 3)
+    dense.forward(X)
+    print(dense.output[:5])
