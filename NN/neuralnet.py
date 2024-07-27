561b6e0fbe507bbd603aa0cc73a633a04cd6f074
miki <miki@miki>
2024-07-27 21:29:29 +0300

    updated commit

---
+++ NN/neuralnet.py
@@ -0,0 +1,11 @@
+import numpy as np
+import nnfs
+from nnfs import spiral_data
+
+class DenseLayer:
+    def __init__(self, ninputs, nneurons):
+        """
+        """
+    def forward():
+        """
+        """
